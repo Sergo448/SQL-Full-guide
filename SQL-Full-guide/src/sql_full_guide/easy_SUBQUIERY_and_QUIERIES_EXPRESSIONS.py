df79cd81cf26fd690bf6ac3c3239e79d4fecee50
Sergo448 <Sergey.6362@mail.ru>
2022-10-12 14:23:21 +0300

    easy_AGGREGATE_funcks.py ++

--- SQL-Full-guide/src/sql_full_guide/easy_SUBQUIERY_and_QUIERIES_EXPRESSIONS.py
+++ SQL-Full-guide/src/sql_full_guide/easy_SUBQUIERY_and_QUIERIES_EXPRESSIONS.py
@@ -106,11 +106,11 @@ quiery_8 = f"SELECT DISTINCT DESCRIPTION FROM PRODUCTS" \
            f"   AND AMOUNT >= 25000.00);"
 
 quiery_8_1 = f"SELECT DISTINCT DESCRIPTION FROM PRODUCTS" \
-           f" WHERE EXISTS " \
-           f"   (SELECT * FROM ORDERS" \
-           f"   WHERE PRODUCT = PRODUCT_ID " \
-           f"   AND MFR = MFR_ID" \
-           f"   AND AMOUNT >= 25000.00);"
+             f" WHERE EXISTS " \
+             f"   (SELECT * FROM ORDERS" \
+             f"   WHERE PRODUCT = PRODUCT_ID " \
+             f"   AND MFR = MFR_ID" \
+             f"   AND AMOUNT >= 25000.00);"
 
 """
                 ANY(), ALL() Многократное сравнение
@@ -158,11 +158,165 @@ quiery_12 = f"SELECT NAME, AGE" \
 quiery_12_1 = f"SELECT NAME, AGE FROM SALESREPS, OFFICES " \
               f" WHERE REP_OFFICE = OFFICE " \
               f" AND REGION = 'Western';"
+quiery_12_2 = f"SELECT NAME, AGE FROM SALESREPS" \
+              f" WHERE EXISTS " \
+              f"    (SELECT * FROM OFFICES" \
+              f"        WHERE REGION = 'Western'" \
+              f"        AND REP_OFFICE = OFFICE);"
+
+"""
+            ВЛОЖЕННЫЕ ПОДЗАПРОСЫ
+"""
+
+# Вывести список клиентов, закрепленных за служащими, работающими в офисах восточного региона
+quiery_13 = f"SELECT COMPANY" \
+            f" FROM CUSTOMERS" \
+            f"  WHERE CUST_REP IN (SELECT EMPL_NUM" \
+            f"                      FROM SALESREPS" \
+            f"                      WHERE REP_OFFICE IN (SELECT OFFICE " \
+            f"                                              FROM OFFICES" \
+            f"                                              WHERE REGION = 'Eastern'));"
+
+"""
+                КОРРЕЛИРОВАННЫЕ ПОДЗАПРОСЫ
+                
+                подзапросы зависят от внешней ссылки (коррелированной ссылки)
+                т.о. такие подзапросы становятся корредированными с каждой
+                строчкой таблицы главного запроса
+                 
+"""
+# Вывести список руководителей старше 40 лет, у которых есть служащие,
+# опережающие план и работающие со своими руководителями в разных офисах
+
+# Для удобства одна и также таблица в основном запросе и подзапросе имеет разные псевдонимы
+quiery_14 = f"SELECT NAME FROM SALESREPS AS MGRS " \
+            f" WHERE AGE > 40 " \
+            f"  AND MGRS.EMPL_NUM IN (SELECT MANAGER FROM SALESREPS AS EMPS" \
+            f"                          WHERE EMPS.SALES > EMPS.QUOTA" \
+            f"                          AND EMPS.REP_OFFICE <> MGRS.REP_OFFICE);"
+
+"""
+                Подзапросы в предложении HAVING*
+"""
+# Вывести список служащих, у которых средняя стоимость заказов на товары, изготовленные
+# компанией ACI, выше, чем общая средняя стоимость заказов
+quiery_15 = f"SELECT NAME, AVG (AMOUNT) FROM SALESREPS, ORDERS" \
+            f" WHERE EMPL_NUM = REP" \
+            f"  AND MFR = 'ACI'" \
+            f"      GROUP BY NAME" \
+            f"  HAVING AVG(AMOUNT) > (SELECT AVG(AMOUNT) FROM ORDERS);"
+
+# Вывести список служащих, у каждого из которых средняя стоимость заказов
+# на товары, изготовленные компанией ACI, не меньше, чем средняя
+# стоимость заказов этого служащего
+quiery_15_1 = f"SELECT NAME, AVG(AMOUNT) FROM SALESREPS, ORDERS" \
+              f" WHERE EMPL_NUM = REP" \
+              f"    AND MFR = 'ACI'" \
+              f"    GROUP BY NAME, EMPL_NUM" \
+              f" HAVING AVG(AMOUNT) >= (SELECT AVG(AMOUNT) FROM ORDERS" \
+              f"                        WHERE REP = EMPL_NUM);"
+
+
+"""
+        CAST(),
+         - Для преобразования данных из таблицы, в которой столбец
+         определен с неверным типом данных
+         - Для приведения возвращаемых запросом данных к типу, 
+         поддерживаемому клиентским приложением
+         - Для устранения отличий типов данных в двух разных таблицах             
+         CASE(),
+          - Для использования синтаксиса подобного If, then, esle...
+          CASE...WHEN...THEN...etc.
+         COALESCE() = easier then case
+         NULLIF()
+         
+"""
+
+quiery_16 = f"SELECT NAME, CAST (REP_OFFICE AS CHAR), " \
+            f"             CAST (HIRE_DATE AS CHAR)" \
+            f"FROM SALESREPS;"
+
+quiery_17 = f"SELECT PRODUCT, QTY, AMOUNT" \
+            f" FROM ORDERS" \
+            f" WHERE CUST = CAST ('2107' AS INTEGER);"
+# Пример. Нам нужны общие объемы продаж служащих по офисам. Если служащему
+# не назначен офис, его сумма должна включаться в итог по офису его руководителя
+quiery_18 = f"SELECT CITY, SUM(SALESREPS.SALES) " \
+            f" FROM OFFICES, SALESREPS" \
+            f"  WHERE OFFICE = CASE WHEN (REP_OFFICE IS NOT NULL) " \
+            f"                      THEN REP_OFFICE " \
+            f"                      ELSE (SELECT REP_OFFICE" \
+            f"                          FROM SALESREPS AS MGRS" \
+            f"                          WHERE MGRS.EMPL_NUM = MANAGER)" \
+            f"                  END" \
+            f"  GROUP BY CITY; "
+
+quiery_19 = f"SELECT NAME, CITY, " \
+            f"      CASE OFFICE WHEN 11 THEN 'New York'" \
+            f"                  WHEN 12 THEN 'Illinois'" \
+            f"                  WHEN 13 THEN 'Georgia'" \
+            f"                  WHEN 21 THEN 'California'" \
+            f"                  WHEN 22 THEN 'Colorado'" \
+            f"      END AS STATE" \
+            f" FROM OFFICES, SALESREPS" \
+            f" WHERE MGR = EMPL_NUM;"
+
+quiery_20 = f"SELECT NAME, CASE WHEN (QUOTA IS NOT 'NULL' OR NULL) THEN QUOTA" \
+            f"                  WHEN (SALES IS NOT NULL OR 'NULL') THEN SALES" \
+            f"                  ELSE 0.00" \
+            f"                  END AS ADJUSTED_QUOTA" \
+            f"  FROM SALESREPS"
+
+quiery_21 = f"SELECT NAME, COALESCE(QUOTA, SALES, 0.00) FROM SALESREPS;"
+quiery_22 = f"SELECT CITY, SUM(SALESREPS.SALES) FROM OFFICES, SALESREPS" \
+            f" WHERE OFFICE = NULLIF (REP_OFFICE, 0)" \
+            f" GROUP BY CITY;"
+
+"""
+                Выражения со строками таблиц
+                (выражение) к какой-то таблице с выбранными строками,
+                 которые совпадают со значениями в выражении
+"""
+
+
+# Перечислить номера заказов на изделие ACI 41002 с указанием количества единиц товара и суммы сделки
+quiery_23 = f"SELECT ORDER_NUM, QTY, AMOUNT FROM ORDERS WHERE (MFR, PRODUCT) = ('ACI', '41002');"
+quiery_23_1 = f"SELECT ORDER_NUM, QTY, AMOUNT FROM ORDERS" \
+              f" WHERE (MFR = 'ACI') AND (PRODUCT = '41002');"
+
+"""
+            Подзапросы, возвращающие строки
+"""
+quiery_24 = f"SELECT MFR_ID, PRODUCT_ID FROM PRODUCTS" \
+            f" WHERE PRICE = (SELECT MAX(PRICE) FROM PRODUCTS);"
+quiery_25 = f"SELECT ORDER_NUM, ORDER_DATE FROM ORDERS" \
+            f" WHERE (MFR, PRODUCT) = (SELECT MFR_ID, PRODUCT_ID FROM PRODUCTS" \
+            f"                          WHERE PRICE = (SELECT MAX(PRICE)" \
+            f"                                         FROM PRODUCTS));"
+
+"""
+                    Табличные выражения
+"""
+
+quiery_26 = f"INSERT INTO OFFICES (OFFICE, CITY, REGION, MGR, SALES)" \
+            f" VALUES (23, 'San Diego', 'Western', 108, 0.00)," \
+            f"        (24, 'Seattle', 'Western', 104, 0.00)," \
+            f"        (14, 'Boston', 'Eastern', NULL, 0.00);"
+quiery_26_1 = f"SELECT * FROM OFFICES;"
+
+# quiery_26 может включать в своей инструкции и подзапрос
+
+quiery_27 = f"SELECT DESCRIPTION, PRICE FROM PRODUCTS" \
+            f" WHERE (MFR_ID, PRODUCT_ID) IN" \
+            f"      (SELECT MFR, PRODUCT FROM ORDERS WHERE AMOUNT > 20000.00);"
+quiery_27_1 = f"SELECT DISTINCT DESCRIPTION, PRICE FROM PRODUCTS, ORDERS" \
+              f" WHERE (MFR_ID = MFR) AND (PRODUCT_ID = PRODUCT) AND (AMOUNT > 20000.00);"
+
 
 
 # Делаем запрос к базе данных, используя обычный SQL-синтаксис
 # cursor.execute(quiery_12)
-cursor.execute(quiery_12_1)
+cursor.execute(quiery_27_1)
 
 # !------------------!------------------!------------------!------------------!------------------!
 # Если мы не просто читаем, но и вносим изменения в базу данных - необходимо сохранить транзакцию
